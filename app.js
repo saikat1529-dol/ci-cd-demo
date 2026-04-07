341c7ea8742f4076872727d9e7c333e72331622f
Fojle Rabbi Saikat <fojle.rabbi@dreamonline.co.jp>
2026-04-07 13:58:43 +0600

    App Js changed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('CI/CD is working!'));
+app.get('/', (req, res) => res.send('CI/CD is working with CD implementation 2!'));
 app.listen(3000, () => console.log('Server ready'));
